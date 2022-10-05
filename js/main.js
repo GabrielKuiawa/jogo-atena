ff20ba04788af7e14de0d16d2829e18a4f9621b2
GabrielKuiawa <gabrielsantos77239@gmail.com>
2022-10-05 20:25:50 -0300

    perguntas aletorias

--- js/main.js
+++ js/main.js
@@ -1,37 +1,59 @@
-var buttons = document.getElementsByClassName('button');
+function listarPerguntas() {
+    var buttons = document.getElementsByClassName('button');
+    
+    var imagem = document.querySelector("#imagem");
+    var ask = document.querySelector("#ask");
+    
+    var button_1 = document.querySelector("#button-1");
+    var button_2 = document.querySelector("#button-2");
+    var button_3 = document.querySelector("#button-3");
+    var button_4 = document.querySelector("#button-4");
+    
 
-var imagem = document.querySelector("#imagem");
-var ask = document.querySelector("#ask");
-
-var button_1 = document.querySelector("#button-1");
-var button_2 = document.querySelector("#button-2");
-var button_3 = document.querySelector("#button-3");
-var button_4 = document.querySelector("#button-4");
-
-
-
-var data = fetch("../js/perguntas/perg.json")
+    var data = fetch("../js/perguntas/perg.json")
     .then(resposta => {return resposta.json()})
     .then(json => {
-        imagem.src = "../" + json[1].imagem;
-        ask.innerHTML = json[1].ask;
-        button_1.innerHTML = json[1].option_1;
-        button_2.innerHTML = json[1].option_2;
-        button_3.innerHTML = json[1].option_3;
-        button_4.innerHTML = json[1].option_4;
+            var random = perguntasAleatorias(0,json.length);
+            existePerguntas.push(random);
+            console.log(existePerguntas);
+            
+            imagem.src = "../" + json[random].imagem;
+            ask.innerHTML = json[random].ask;
+            button_1.innerHTML = json[random].option_1;
+            button_2.innerHTML = json[random].option_2;
+            button_3.innerHTML = json[random].option_3;
+            button_4.innerHTML = json[random].option_4;
+            
+            // Array.prototype.slice.call(buttons).forEach(function(pegaElementoAtual){
+            //     pegaElementoAtual.addEventListener('click', function(e){
+            //         if (this == button_1) {
+            //             alert("ACERTOU");
+            //         } else {
+            //             alert("errou");
+            //         }
+            //     });
+            // });
+            
+            
+        }
+    );
+        
+}
+    
+function perguntasAleatorias(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+    
+var existePerguntas = [];
+// console.log(existePerguntas);
+// existePerguntas.push(1);
+// existePerguntas.push(2)
+// console.log(existePerguntas);
+// console.log(perguntasAleatorias(1,3));
 
-        Array.prototype.slice.call(buttons).forEach(function(pegaElementoAtual){
-            pegaElementoAtual.addEventListener('click', function(e){
-                if (this == button_1) {
-                    alert("ACERTOU");
-                } else {
-                    alert("errou");
-                }
-            });
-        });
-
-
-    }
-);
+listarPerguntas();
+// listarPerguntas();
 
 // console.log(data);
